efddb30e52cbb27892c97bbec6529967e45122a0
yanglinguan <l69yang@uwaterloo.ca>
2017-08-08 21:32:05 -0400

    react front end news panel, newscard

---
+++ week5/web_server/client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import './index.css';
+
+// react app entry point
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
