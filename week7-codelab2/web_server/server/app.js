2e0d3878c6e8db48b1bc53195329db4779b46a7a
yanglinguan <l69yang@uwaterloo.ca>
2017-08-19 10:19:08 -0400

    auth server

--- week7-codelab2/web_server/server/app.js
+++ week7-codelab2/web_server/server/app.js
@@ -1,8 +1,12 @@
+var bodyParser = require('body-parser')
 var config = require('./config/config.json');
 var cors = require('cors');
 var express = require('express');
+var passport = require('passport');
 var path = require('path');
 
+// routers
+var auth = require('./routes/auth');
 var index = require('./routes/index');
 var news = require('./routes/news');
 
@@ -16,6 +20,13 @@ app.set('views', path.join(__dirname, '../client/build/'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
 
+
+app.use(passport.initialize());
+var localSignupStrategy = require('./password/signup_passort');
+var localLoginStrategy = require('./password/login_passport');
+passport.use('local-signup', localSignupStrategy);
+passport.use('locak-login', localLoginStrategy);
+
 // TODO: remove after development is done
 // app.all('*', function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -23,8 +34,11 @@ app.use('/static', express.static(path.join(__dirname, '../client/build/static/'
 //   next();
 // })
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use('/', index);
+app.use('/auth', auth);
+var authCheckerMiddleware = require('./middleware/auth_checker');
+app.use('/', authCheckerMiddleware);
 app.use('/news', news);
 
 // catch 404 and forward to error handler
--- week7-codelab2/web_server/server/routes/index.js
+++ week7-codelab2/web_server/server/routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
